e1fed9abb7c748d3eb4adc9a3a01c552fa5ab562
Mo <101009703+Mo31D@users.noreply.github.com>
2026-09-15 14:31:43 +0100

    Refresh cache after login keyboard fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='sozan-v3-2';
+const CACHE='sozan-v3-3';
 const STATIC=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
 
 self.addEventListener('install',event=>{
